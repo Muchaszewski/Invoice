eb867dc899a885fa8af2fbcb252a0ccf3b42f436
Muchaszewski <99999deuter@gmail.com>
2015-01-19 22:31:04 +0100

    Backup Google fix

---
+++ Invoice/Utils/GoogleBackup/BackupMenu.cs
@@ -0,0 +1,77 @@
+﻿using Faktury.Utils;
+using Faktury.Utils.Backup;
+using Google.Apis.Drive.v2.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Faktury.Options
+{
+    public partial class BackupMenu : Form
+    {
+        static public List<BackupItems> listGoogle = new List<BackupItems>();
+        
+        public BackupMenu()
+        {
+            InitializeComponent();
+            ItemsRebuild();
+            if(Program.GoogleAPI == false)
+            {
+                buttonNewGoogle.Enabled = false;
+                buttonRestoreGoogle.Enabled = false;
+            }
+        }
+
+        public void ItemsRebuild()
+        {
+            int counter = 0;
+            if (Program.GoogleAPI)
+            {
+                listGoogle = GoogleDriveBackup.GetBackupData();
+                listView1.Items.Clear();
+                foreach (var item in listGoogle)
+                {
+                    string[] row = { "Google Drive", item.File.CreatedDate.ToString(), item.InvoiceCount.ToString(), item.ConfigCount.ToString(), item.InvoiceData, item.ContractorCount.ToString(), item.ItemCount.ToString() };
+                    var listViewItem = new ListViewItem(row, counter++);
+                    listView1.Items.Add(listViewItem);
+                }
+            }
+        }
+
+        private void buttonNewGoogle_Click(object sender, EventArgs e)
+        {
+            GoogleDriveBackup.Make();
+            GoogleDriveBackup.Load();
+            ItemsRebuild();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            if (Program.GoogleAPI)
+            {
+                GoogleDriveBackup.Load();
+            }
+            ItemsRebuild();
+        }
+
+        private void buttonRestoreGoogle_Click(object sender, EventArgs e)
+        {
+            int index = listView1.FocusedItem.Index;
+            DialogResult result = MessageBox.Show("Czy napewno chcesz wczytać pliki z dnia: "+ listView1.FocusedItem.SubItems[1]+" z dysku Google?" , "Przywracanie danych", MessageBoxButtons.YesNo);
+            if(result == System.Windows.Forms.DialogResult.Yes)
+            {
+               FileWriter.Replace(FileWriter.ITEMS_LIST, GoogleDriveBackup.DownloadFile(listGoogle[index].Items));
+               FileWriter.Replace(FileWriter.CONFIG, GoogleDriveBackup.DownloadFile(listGoogle[index].Config));
+               FileWriter.Replace(FileWriter.CONTRACTORS_LIST, GoogleDriveBackup.DownloadFile(listGoogle[index].Contractors));
+               FileWriter.Replace(FileWriter.INVOICES_LIST, GoogleDriveBackup.DownloadFile(listGoogle[index].Invoice));
+               Application.Restart();
+            }
+        }
+    }
+}
---
+++ Invoice/Utils/GoogleBackup/GoogleDriveBackup.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Google;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v2;
+using Google.Apis.Drive.v2.Data;
+using Google.Apis.Services;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Net;
+using Faktury.Options;
+
+
+namespace Faktury.Utils.Backup
+{
+    public static class GoogleDriveBackup
+    {
+        private static string MimeFolder = "application/vnd.google-apps.folder";
+
+        private static UserCredential credential;
+        private static DriveService service;
+        private static DateTime id;
+        private static List<File> driveContent;
+        /// <summary>
+        /// mod 5 = 0 items
+        /// mod 5 = 1 config
+        /// mod 5 = 2 contractors
+        /// mod 5 = 3 invoice
+        /// mod 5 = 4 folder
+        /// </summary>
+        public static List<File> DriveContent
+        {
+            get { return GoogleDriveBackup.driveContent; }
+        }
+
+        /// <summary>
+        /// Establish connection with google drive with given ClientID and ClientSecrets
+        /// </summary>
+        public static void Connect()
+        {
+            credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = "176534462054-dvkjkjrgqs7maek51oql45oaonuqoh36.apps.googleusercontent.com",
+                    ClientSecret = "SCpCI8mC52OeZMsm5kKbiK84",
+                },
+                new[] { DriveService.Scope.Drive },
+                "Faktury",
+                CancellationToken.None).Result;
+
+            // Create the service.
+            service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Invoice Backup Drive",
+            });
+
+            Load();
+        }
+
+
+        public static void Make()
+        {
+            id = DateTime.Now;
+            string folder = "Backup " + id;
+
+            TextWindow textWindow = new TextWindow(folder);
+            textWindow.Show();
+            textWindow.DialogText.Text = "Tworzenie folderu";
+            File folderFile = CreateFolder(folder);
+
+            textWindow.DialogText.Text = "Wysyłanie pliku INVOICE";
+            insertFile("Invoice", Data.Invoices.Count + "_" + FileWriter.getModificationDate(FileWriter.INVOICES_LIST), folderFile.Id, "text/xml", FileWriter.INVOICES_LIST);
+
+            textWindow.DialogText.Text = "Wysyłanie pliku CONTRACTORS";
+            insertFile("Contractors", Data.Contractors.Count + "_" + FileWriter.getModificationDate(FileWriter.CONTRACTORS_LIST), folderFile.Id, "text/xml", FileWriter.CONTRACTORS_LIST);
+
+            textWindow.DialogText.Text = "Wysyłanie pliku CONFIG";
+            insertFile("Config", Data.Config.InvoiceNumer + "_" + FileWriter.getModificationDate(FileWriter.CONFIG), folderFile.Id, "text/xml", FileWriter.CONFIG);
+
+            textWindow.DialogText.Text = "Wysyłanie pliku ITEMS";
+            insertFile("Items", Data.Items.Count + "_" + FileWriter.getModificationDate(FileWriter.ITEMS_LIST), folderFile.Id, "text/xml", FileWriter.ITEMS_LIST);
+            textWindow.Close();
+        }
+
+        /// <summary>
+        /// Create new folder
+        /// </summary>
+        /// <param name="folderId">Folder name</param>
+        /// <param name="parentId">Parent folder; default root</param>
+        /// <returns>Folder metadata, null is returned if an API error occurred</returns>
+        public static File CreateFolder(string folderId, string parentId = "root")
+        {
+            File body = new File();
+            body.Title = folderId;
+            body.Description = "Folder";
+            body.MimeType = MimeFolder;
+            body.Parents = new List<ParentReference>() { new ParentReference() { Id = parentId } };
+            try
+            {
+                FilesResource.InsertRequest request = service.Files.Insert(body);
+                return request.Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Insert new file.
+        /// </summary>
+        /// <param name="service">Drive API service instance.</param>
+        /// <param name="title">Title of the file to insert, including the extension.</param>
+        /// <param name="description">Description of the file to insert.</param>
+        /// <param name="parentId">Parent folder's ID.</param>
+        /// <param name="mimeType">MIME type of the file to insert.</param>
+        /// <param name="filename">Filename of the file to insert.</param>
+        /// <returns>Inserted file metadata, null is returned if an API error occurred.</returns>
+        private static File insertFile(string title, string description, string parentId, string mimeType, string filename)
+        {
+            // File's metadata.
+            File body = new File();
+            body.Title = title;
+            body.Description = description;
+            body.MimeType = mimeType;
+
+            // Set the parent folder.
+            if (!String.IsNullOrEmpty(parentId))
+            {
+                body.Parents = new List<ParentReference>() { new ParentReference() { Id = parentId } };
+            }
+
+            // File's content.
+            byte[] byteArray = System.IO.File.ReadAllBytes(filename);
+            System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
+            try
+            {
+                FilesResource.InsertMediaUpload request = service.Files.Insert(body, stream, mimeType);
+                try
+                {
+                    request.Upload();
+
+                    File file = request.ResponseBody;
+                    // Uncomment the following line to print the File ID.
+                    //Console.WriteLine("File ID: " + file.Id);
+
+                    return file;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred in Insert: " + e.Message);
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+                return null;
+            }
+        }
+
+
+        public static void Load()
+        {
+            List<File> result = new List<File>();
+            FilesResource.ListRequest request = service.Files.List();
+
+            TextWindow textWindow = new TextWindow("Wczytywanie danych z Google Drive");
+            textWindow.Show();
+            textWindow.DialogText.Text = "Wczytywanie danych z Google Drive";
+            do
+            {
+                try
+                {
+                    FileList files = request.Execute();
+                    result.AddRange(files.Items);
+                    request.PageToken = files.NextPageToken;
+                }
+                catch (Exception e)
+                {
+                    textWindow.DialogText.Text = "BŁĄD!";
+                    Console.WriteLine("An error occurred: " + e.Message);
+                    request.PageToken = null;
+                }
+            } while (!String.IsNullOrEmpty(request.PageToken));
+            textWindow.Close();
+            driveContent = result;
+        }
+
+        public static List<BackupItems> GetBackupData()
+        {
+            List<BackupItems> result = new List<BackupItems>();
+            int index = 0;
+            foreach (var item in DriveContent)
+            {
+                if (result.Count > 0)
+                {
+                    if (item.MimeType == MimeFolder)
+                    {
+                        index = result.FindIndex(i => i.FolderID == item.Id);
+                    }
+                    else
+                    {
+                        index = result.FindIndex(i => i.FolderID == item.Parents[0].Id);
+                    }
+                    if (index == -1)
+                    {
+                        if (item.Parents[0].IsRoot == false)
+                        {
+                            result.Add(new BackupItems(item.Parents[0].Id));
+                        }
+                        else
+                        {
+                            result.Add(new BackupItems(item.Id));
+                        }
+                        index = result.Count - 1;
+                    }
+                }
+                else
+                {
+                    if (item.Parents[0].IsRoot == false)
+                    {
+                        result.Add(new BackupItems(item.Parents[0].Id));
+                    }
+                    else
+                    {
+                        result.Add(new BackupItems(item.Id));
+                    }
+                }
+
+                if (item.MimeType == MimeFolder)
+                {
+                    result[index].File = item;
+                    var a = result[index].File.Id;
+                }
+                else
+                {
+                    if (item.Title == "Invoice")
+                    {
+                        string[] lines = Regex.Split(item.Description, "_");
+                        result[index].InvoiceCount = Convert.ToInt32(lines[0]);
+                        result[index].InvoiceData = lines[1];
+                        result[index].Invoice = item;
+                    }
+                    else if (item.Title == "Contractors")
+                    {
+                        string[] lines = Regex.Split(item.Description, "_");
+                        result[index].ContractorCount = Convert.ToInt32(lines[0]);
+                        result[index].ContractorData = lines[1];
+                        result[index].Contractors = item;
+                    }
+                    else if (item.Title == "Items")
+                    {
+                        string[] lines = Regex.Split(item.Description, "_");
+                        result[index].ItemCount = Convert.ToInt32(lines[0]);
+                        result[index].ItemData = lines[1];
+                        result[index].Items = item;
+                    }
+                    else if (item.Title == "Config")
+                    {
+                        string[] lines = Regex.Split(item.Description, "_");
+                        result[index].ConfigCount = Convert.ToInt32(lines[0]);
+                        result[index].ConfigData = lines[1];
+                        result[index].Config = item;
+                    }
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Download a file and return a string with its content.
+        /// </summary>
+        /// <param name="file">Drive File instance.</param>
+        /// <returns>File's content if successful, null otherwise.</returns>
+        public static byte[] DownloadFile(File file)
+        {
+            if (!String.IsNullOrEmpty(file.DownloadUrl))
+            {
+                try
+                {
+                    TextWindow textWindow = new TextWindow("Wczytywanie danych z Google Drive");
+                    textWindow.Show();
+                    textWindow.DialogText.Text = "Pobieranie danych";
+                    var item = service.Files.Get(file.Id).Execute();
+                    textWindow.Close();
+                    return service.HttpClient.GetByteArrayAsync(file.DownloadUrl).Result;
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("An error occurred: " + e.Message);
+                    return null;
+                }
+            }
+            else
+            {
+                // The file doesn't have any content stored on Drive.
+                return null;
+            }
+        }
+    }
+}
